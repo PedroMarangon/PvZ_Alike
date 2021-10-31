3256c751fe050fa1ab02e5a010bced70209d6482
Pedro Marangon <pedrommarangon@gmail.com>
2021-10-31 19:05:33 -0300

    Grid now initializes itself

--- Assets/_Project/Scripts/GridSetup.cs
+++ Assets/_Project/Scripts/GridSetup.cs
@@ -11,40 +11,72 @@ namespace S2P_Test
 		[Header("Grid generation")]
 		[SerializeField] private Vector2Int gridSize = new Vector2Int(3, 2);
 		[Min(1), SerializeField] private float cellSize = 1;
-		[SerializeField] private Color DEBUG_c1 = new Color(255, 255, 255, 0.75f);
-		[SerializeField] private Color DEBUG_c2 = new Color(0, 0, 0, 0.75f);
+		[SerializeField] private GridPiece gridPiecePrefab;
+		[SerializeField] private Color color01 = Color.white;
+		[SerializeField] private Color color02 = Color.black;
+
 
 		[Header("Spawner generation")]
-		[SerializeField] private EnemySpawner enemySpawnerPrefab;
 		[SerializeField] private float spawnerXPos = -10;
+		[SerializeField] private EnemySpawner enemySpawnerPrefab;
+
+		private Dictionary<Vector2Int, GridPiece> instantiatedGrid = new Dictionary<Vector2Int, GridPiece>();
 
 		private void Awake()
 		{
-			if(enemySpawnerPrefab)
-			{
-				for (int y = 0; y < gridSize.y; y++)
-				{
-					Vector3 point = transform.position;
-					point.z += y * cellSize;
-					point.x += spawnerXPos;
+			CreateGrid();
 
-					GameObject instantiatedSpawner = Instantiate(enemySpawnerPrefab, point, Quaternion.identity).gameObject;
-					instantiatedSpawner.name = $"{enemySpawnerPrefab.name} - ROW {y+1}";
-				}
-			}
+			CreateSpawners();
 		}
 
-
-		private void OnDrawGizmos()
+		private void CreateGrid()
 		{
-			Vector3 cellSizeVector = new Vector3(cellSize, 0, cellSize);
-			
+			if (!gridPiecePrefab) return;
 
 			for (int x = 0; x < gridSize.x; x++)
 			{
 				for (int y = 0; y < gridSize.y; y++)
 				{
-					Gizmos.color = ((x + y) % 2 == 1) ? DEBUG_c2 : DEBUG_c1;
+					Vector3 point = transform.position + (new Vector3(x, 0, y) * cellSize);
+
+					GridPiece gridPiece = Instantiate(gridPiecePrefab, point, Quaternion.identity).GetComponent<GridPiece>();
+					gridPiece.Init(cellSize, ((x + y) % 2 == 1) ? color02 : color01);					
+					
+					Vector2Int gridPos = new Vector2Int(x, y);
+					
+					instantiatedGrid.Add(gridPos, gridPiece);
+
+				}
+			}
+
+
+		}
+
+		private void CreateSpawners()
+		{
+			if (!enemySpawnerPrefab) return;
+
+			for (int y = 0; y < gridSize.y; y++)
+			{
+				Vector3 point = transform.position;
+				point.z += y * cellSize;
+				point.x += spawnerXPos;
+
+				GameObject instantiatedSpawner = Instantiate(enemySpawnerPrefab, point, Quaternion.identity).gameObject;
+				instantiatedSpawner.name = $"{enemySpawnerPrefab.name} - ROW {y + 1}";
+			}
+		}
+
+#if UNITY_EDITOR
+		private void OnDrawGizmos()
+		{
+			if (UnityEditor.EditorApplication.isPlaying) return;
+			Vector3 cellSizeVector = new Vector3(cellSize, 0, cellSize);
+			for (int x = 0; x < gridSize.x; x++)
+			{
+				for (int y = 0; y < gridSize.y; y++)
+				{
+					Gizmos.color = ((x + y) % 2 == 1) ? color02 : color01;
 					Vector3 point = transform.position + (new Vector3(x, 0, y) * cellSize);
 					Gizmos.DrawCube(point + (Vector3.up * 0.001f), cellSizeVector);
 
@@ -56,9 +88,8 @@ namespace S2P_Test
 
 				}
 			}
-
-
 		}
+#endif
 
 	}
 }
